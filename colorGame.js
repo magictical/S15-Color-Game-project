bc11da8d0d3efa3a79eb18bb335ceeecfc1d828f
Ian <magictical@gmail.com>
2018-02-12 17:48:53 +0900

    display the message when user get the correct answer. after h1

--- colorGame.js
+++ colorGame.js
@@ -8,13 +8,14 @@ var colors = [
 ];
 //모든 square 선택
 var square = document.querySelectorAll(".square");
-
 // the RGB value will be displayed on h1
 var targetColor = colors[3];
 //select span in h1 by id
 var displayRGB = document.getElementById("colorDisplay");
 //set RGB value in the span
 displayRGB.textContent = targetColor;
+
+var messageDisplay = document.getElementById("message");
 
 // for 문으로 iterate하며 color 부여
 for(var i = 0; i < square.length; i++) {
@@ -26,12 +27,11 @@ for(var i = 0; i < square.length; i++) {
       //클릭하면 해당 배열의 RGB값을 리턴
       var pickedColor =this.style.backgroundColor;
       if(pickedColor === targetColor) {
-        alert("Correct!");
+        messageDisplay.textContent = "Correct!";
       } else {
         //답이 틀린경우 해당 square를 없앤다.
         this.style.backgroundColor = "#232323";
-
-        //틀린경우 tray again 출력
+        messageDisplay.textContent = "Try Again";
       }
   });
 }
